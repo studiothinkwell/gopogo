5b5cb79827d909d41d6316be712d2f43e987a61b
techdharma-yogesh <yogesh@techdharma.com>
2010-12-09 18:46:10 +0530

    Test

--- tests/application/bootstrap.php
+++ tests/application/bootstrap.php
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-mahesh nnnn
-=======
-This is just a modified test line again ,peer here werwerSSS
->>>>>>> c7be90b0235868749d2a16be084276cd085fac50
+gfsdfs fsdfs dfsd sdfsdf sdf sdf
+sdfsdfsdf
